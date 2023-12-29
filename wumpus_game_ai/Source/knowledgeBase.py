94ec5ff62545c1b5c276b6fa34135aaba8b36097
Nguyễn Chí Cường <cuongnguyengl2003@gmail.com>
2023-12-29 22:48:28 +0700

    a

--- wumpus_game_ai/Source/knowledgeBase.py
+++ wumpus_game_ai/Source/knowledgeBase.py
@@ -1,56 +1,41 @@
-from pysat.solvers import Glucose3
 import copy
 
 class KnowledgeBase:
     def __init__(self):
+        """Khởi tạo KnowledgeBase với một bảng kiến thức trống (KB)."""
         self.KB = []
-
 
     @staticmethod
     def standardize_clause(clause):
+        """ Chuẩn hóa một mệnh đề bằng cách sắp xếp và loại bỏ trùng lặp."""
         return sorted(list(set(clause)))
 
-
     def add_clause(self, clause):
+        """Thêm một mệnh đề vào KB, trước tiên Chuẩn hóa mệnh đề và kiểm tra sự tồn tại."""
         clause = self.standardize_clause(clause)
         if clause not in self.KB:
             self.KB.append(clause)
 
-
     def del_clause(self, clause):
+        """Xóa một mệnh đề khỏi KB, sau tiêu chuẩn hóa và kiểm tra sự tồn tại."""
         clause = self.standardize_clause(clause)
         if clause in self.KB:
             self.KB.remove(clause)
 
-
     def infer(self, not_alpha):
-        # Create a deep copy of the knowledge base
-        extended_kb = copy.deepcopy(self.KB)
-        
-        # Add the negated clause not_alpha to the extended knowledge base
-        extended_kb.append([not_alpha])
-        
-        # Check each clause in the extended knowledge base
-        for clause in extended_kb:
-            # Check if the clause is valid
-            is_valid = self.is_clause_valid(clause, extended_kb)
-            
-            # If any clause is invalid, return False
-            if not is_valid:
-                return False
-        
-        # If all clauses are valid, return True
-        return True
-    
-    def is_clause_valid(self, clause, extended_kb):
-        # Check if a clause can be satisfied
-        for literal in clause[0]:  # Extract the literals from the inner list
-            # If a positive literal is not negated and not in the extended knowledge base, return True
-            if literal > 0 and [-literal] not in extended_kb:
+        """Kiểm tra tính suy luận: nếu KB và not_alpha đồng thời không hợp lệ thì trả về True, ngược lại trả về False."""
+        # Để đơn giản hóa, chúng ta chỉ kiểm tra xem có một mệnh đề trong KB mà không hợp lệ hay không
+        print('not_alpha', not_alpha)
+        for clause in self.KB:
+            if self.is_unsatisfiable(clause, not_alpha):
                 return True
-            # If a negative literal is not in the extended knowledge base, return True
-            elif literal < 0 and [-literal] not in extended_kb:
+        return False
+
+    def is_unsatisfiable(self, clause1, clause2):
+        """Kiểm tra xem hai mệnh đề có không thể đồng thời đúng hay không."""
+        # Đơn giản là so sánh từng phần tử trong hai mệnh đề
+        for literal in clause1:
+            print('c', [-literal])
+            if -literal in clause2:  # -literal là phủ định của literal
                 return True
-        
-        # If no valid cases found, return False
         return False
